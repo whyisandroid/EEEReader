d277ee620d7099311d815e28474704256e4e2983
jooking <309928507@qq.com>
2016-02-18 16:01:19 +0800

    分享和笔记

---
+++ app/src/main/java/com/ereader/client/ui/adapter/NoteAdapter.java
@@ -0,0 +1,32 @@
+package com.ereader.client.ui.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by ghf on 16/2/18.
+ */
+public class NoteAdapter extends BaseAdapter{
+
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
